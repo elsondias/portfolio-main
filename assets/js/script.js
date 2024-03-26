bd564c48182647dba2ab7111ecf50fb5db6eb40d
Elson Dias <102491017+elsondias@users.noreply.github.com>
2024-03-25 22:09:27 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,3 @@
-let btnMenu = document.getElementById("btn-menu")
-let menu = document.getElementById("menu-mobile")
-let overlay = document.getElementById("overlay-menu")
-
-btnMenu.addEventListener("click", () => {
-    menu.classList.add("abrir-menu")
-})
-
-menu.addEventListener("click", () => {
-    menu.classList.remove("abrir-menu")
-})
-
-overlay.addEventListener("click", () => {
-    menu.classList.remove("abrir-menu")
-})
-
 onload = function () {
     window.onscroll = function () { scrollFunction() };
 };
@@ -27,8 +11,8 @@ function scrollFunction() {
 }
 
 function topFunction() {
-    document.body.scrollTop = 0; 
-    document.documentElement.scrollTop = 0; 
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 toggle.addEventListener("change", () => {
